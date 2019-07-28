faab214d888297d376a2aaee89f906852b8664b0
Dong Han <dong.han@points.org>
2019-07-28 11:45:18 +0800

    fix no video issue after system sleep

--- app/src/main/java/cn/turboshow/tv/TBSPlayer.kt
+++ app/src/main/java/cn/turboshow/tv/TBSPlayer.kt
@@ -6,23 +6,20 @@ import org.videolan.libvlc.LibVLC
 import org.videolan.libvlc.MediaPlayer
 import org.videolan.libvlc.util.VLCVideoLayout
 
-class TBSPlayer(context: Context, videoView: VLCVideoLayout) {
+class TBSPlayer(context: Context, private val videoView: VLCVideoLayout) {
     private val options = arrayListOf("--vout=android_display,none")
     private val libVLC = LibVLC(context, options)
-    private val player: MediaPlayer
-
-    init {
-        player = MediaPlayer(libVLC)
-        player.attachViews(videoView, null, false, false)
-        player.videoScale = MediaPlayer.ScaleType.SURFACE_16_9
-    }
+    private val player = MediaPlayer(libVLC)
 
     fun play(uri: Uri) {
+        player.attachViews(videoView, null, false, false)
+        player.videoScale = MediaPlayer.ScaleType.SURFACE_16_9
         player.play(uri)
     }
 
     fun stop() {
         player.stop()
+        player.detachViews()
     }
 
     fun release() {
