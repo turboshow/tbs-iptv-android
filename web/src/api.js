14071ecbe3b90f8df9ed8069628a26ded394ad4c
Dong Han <dong.han@points.org>
2020-05-01 14:48:43 +0800

    move web ui source to standalone project

--- web/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function fetchUdpxySettings() {
-  return axios.get('/api/settings/udpxy').then(resp => resp.data);
-}
-
-export function updateUdpxySettings(settings) {
-  return axios.post('/api/settings/udpxy', settings);
-}
