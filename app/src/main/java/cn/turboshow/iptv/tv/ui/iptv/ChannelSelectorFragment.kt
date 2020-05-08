19642d4c841b270b5e0c360011cb489be6f9761d
Dong Han <dong.han@points.org>
2020-05-08 23:33:30 +0800

    use multi-column channel menu

--- app/src/main/java/cn/turboshow/iptv/tv/ui/iptv/ChannelSelectorFragment.kt
+++ app/src/main/java/cn/turboshow/iptv/tv/ui/iptv/ChannelSelectorFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.leanback.widget.OnItemViewClickedListener
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import cn.turboshow.iptv.tv.AppViewModel
 import cn.turboshow.iptv.tv.R
 import cn.turboshow.iptv.tv.di.activityViewModelProvider
+import cn.turboshow.iptv.tv.model.Channel
 import dagger.android.support.DaggerDialogFragment
 import kotlinx.android.synthetic.main.fragment_channels.*
 import javax.inject.Inject
@@ -37,13 +40,14 @@ class ChannelSelectorFragment : DaggerDialogFragment() {
     }
 
     private fun initChannelsView() {
-        viewModel.channels.observe(this, androidx.lifecycle.Observer {
-            channelsView.adapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, it)
-            channelsView.setOnItemClickListener { _, _, position, _ ->
-                viewModel.selectChannel(position)
-                dismiss()
+        viewModel.channels.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            channelsView.adapter = ChannelsAdapter(it).apply {
+                onItemViewClickedListener = OnItemViewClickedListener { _, channel, _, _ ->
+                    viewModel.selectChannel(it.indexOf(channel))
+                    dismiss()
+                }
             }
-            channelsView.post { channelsView.setSelection(it.indexOf(viewModel.currentChannel.value)) }
+            channelsView.post { channelsView.scrollToPosition(it.indexOf(viewModel.currentChannel.value)) }
         })
     }
 
@@ -52,3 +56,36 @@ class ChannelSelectorFragment : DaggerDialogFragment() {
         fun newInstance() = ChannelSelectorFragment()
     }
 }
+
+class ChannelsAdapter(private val channels: List<Channel>) :
+    RecyclerView.Adapter<ChannelsAdapter.ViewHolder>() {
+    var onItemViewClickedListener: OnItemViewClickedListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_channel, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return channels.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val channel = channels[position]
+        holder.setTitle(channel.title)
+        holder.itemView.setOnClickListener {
+            if (onItemViewClickedListener != null) {
+                onItemViewClickedListener!!.onItemClicked(null, channel, null, null)
+            }
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setTitle(title: String) {
+            (itemView as TextView).text = title
+        }
+    }
+}
+
